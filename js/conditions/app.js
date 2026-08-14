b30f173c99f0ab58ed9d144b7b6cf947d06dc0ba
Blueonn33 <m.marinov1733@gmail.com>
2026-08-14 12:01:07 +0300

    get in

--- js/conditions/app.js
+++ js/conditions/app.js
@@ -1,13 +1,39 @@
-console.log("Before conditional");
-let rand = Math.floor(Math.random() * 10) + 1;
+// console.log("Before conditional");
+// let rand = Math.floor(Math.random() * 10) + 1;
 
-if (rand < 5) {
-    console.log("Your num is less than 5");
-    console.log(rand);
-}
-if (rand >= 5) {
-    console.log("Your num is greater than 5");
-    console.log(rand);
-}
+// if (rand < 5) {
+//     console.log("Your num is less than 5");
+//     console.log(rand);
+// }
+// if (rand >= 5) {
+//     console.log("Your num is greater than 5");
+//     console.log(rand);
+// }
 
-console.log("After conditional");
+// console.log("After conditional");
+
+// const dayOfWeek = "Monday";
+// if (dayOfWeek == "Monday") {
+//     console.log("I love Mondays");
+// } else if (dayOfWeek == "Saturday") {
+//     console.log("YaY");
+// } else if (dayOfWeek == "Friday") {
+//     console.log("Yeahhh");
+// }
+
+0 - 5   - FREE 
+6 - 10  - $10
+11 - 65 - $20
+66+     - $10   
+
+const age = 8;
+
+if (age <= 5) {
+    console.log("You are a baby. GET IN");
+} else if (age <= 10) {
+    console.log("You are a child");
+} else if (age <= 65) {
+    console.log("You are an adult");
+} else {
+    console.log("You are old. GET IN, baby");
+}
