e0f9cbb4aa5ffba33f8a7ef2d667f0fa24f8d308
Blueonn33 <m.marinov1733@gmail.com>
2026-08-14 12:25:54 +0300

    pass word s

--- js/conditions/app.js
+++ js/conditions/app.js
@@ -3,37 +3,50 @@
 
 // if (rand < 5) {
 //     console.log("Your num is less than 5");
-//     console.log(rand);
-// }
-// if (rand >= 5) {
+// } else {
 //     console.log("Your num is greater than 5");
-//     console.log(rand);
 // }
+// console.log(rand);
 
 // console.log("After conditional");
 
-// const dayOfWeek = "Monday";
-// if (dayOfWeek == "Monday") {
+// const dayOfWeek = prompt("Enter a day").toLowerCase();
+
+// if (dayOfWeek == "monday") {
 //     console.log("I love Mondays");
-// } else if (dayOfWeek == "Saturday") {
+// } else if (dayOfWeek == "saturday") {
 //     console.log("YaY");
-// } else if (dayOfWeek == "Friday") {
+// } else if (dayOfWeek == "friday") {
 //     console.log("Yeahhh");
+// } else {
+//     console.log("MEH");
 // }
 
-0 - 5   - FREE 
-6 - 10  - $10
-11 - 65 - $20
-66+     - $10   
+// 0 - 5   - FREE
+// 6 - 10  - $10
+// 11 - 65 - $20
+// 66+     - $10
 
-const age = 8;
+// const age = 8;
 
-if (age <= 5) {
-    console.log("You are a baby. GET IN");
-} else if (age <= 10) {
-    console.log("You are a child");
-} else if (age <= 65) {
-    console.log("You are an adult");
+// if (age <= 5) {
+//     console.log("You are a baby. GET IN");
+// } else if (age <= 10) {
+//     console.log("You are a child");
+// } else if (age <= 65) {
+//     console.log("You are an adult");
+// } else {
+//     console.log("You are old. GET IN, baby");
+// }
+
+const password = prompt("Please enter a new password");
+
+if (password.length >= 6) {
+  if (password.indexOf(" ") === -1) {
+    console.log("Valid password, nice");
+  } else {
+    console.log("Remove the space, Knigga");
+  }
 } else {
-    console.log("You are old. GET IN, baby");
+  console.log("Password is too short!! 6+ chars, Knigga :)");
 }
