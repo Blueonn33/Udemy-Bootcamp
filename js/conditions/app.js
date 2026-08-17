9b7d9fa49c13f6270288fa3f4178ce00690e108c
Blueonn33 <m.marinov1733@gmail.com>
2026-08-17 23:15:57 +0300

    switch break make cake sake nake

--- js/conditions/app.js
+++ js/conditions/app.js
@@ -61,7 +61,29 @@ if (userInput) {
 
 const password = prompt("Enter your password");
 if (password.length >= 6 && password.indexOf(" ") === -1) {
-    console.log("Valid");
+  console.log("Valid");
 } else {
-    console.log("Invalid pass, Knigga");
+  console.log("Invalid pass, Knigga");
+}
+
+const day = 2;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  default:
+    console.log("Почивкааа");
+    break;
 }
