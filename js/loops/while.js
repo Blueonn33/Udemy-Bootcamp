7069d58322ca0b92f0e04047c3fff37707436951
Blueonn33 <m.marinov1733@gmail.com>
2026-08-24 21:57:30 +0300

    guess the num

--- js/loops/while.js
+++ js/loops/while.js
@@ -4,3 +4,12 @@ while (count < 10) {
     console.log(count);
     count++;
 }
+
+const secret = "NiggaMigga1119";
+let code = prompt("enter the secret code...");
+
+while (code !== secret){
+    code = prompt("enter the secret code...");
+}
+
+console.log("Congrats, you got the secret"); 
