b3d7fabe52d6b5252cdee6a6fa92bf8ba87d7940
Blueonn33 <m.marinov1733@gmail.com>
2026-08-24 20:38:27 +0300

    nested chairs

--- js/loops/index.js
+++ js/loops/index.js
@@ -9,3 +9,37 @@ for (let i = 0; i <= 20; i += 2) {
 for (let i = 100; i >= 0; i -= 10){
     console.log(i);
 }
+
+for (let i = 20; i >= 0; i++) {
+    // No. . . 
+    //      -
+}
+
+const animals = ["lion", "dino", "cat", "tiger", "nigga", "dog"];
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`i is: ${i}`);
+
+    for (let j = 1; j <= 4; j++) {
+        console.log(`   j is: ${j}`);
+    }
+}
+
+const seatingChart = [
+    ["Martin", "Raya", "---"],
+    ["---", "Jovanni", "Purreto"],
+    ["Bocacho", "Muchacho", "Elnino"]
+];
+
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`ROW: ${i + 1}`);
+    
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j]);
+    }
+}
