af24a344e9d7a0bf77740f37756a459edb0d32c5
Blueonn33 <m.marinov1733@gmail.com>
2026-08-26 10:44:59 +0300

    functions, blocks

---
+++ js/scope/index.js
@@ -0,0 +1,44 @@
+// FUNCTION SCOPE
+let totalEggs = 0;
+
+function collectEggs() {
+    let totalEggs = 5;
+    console.log(totalEggs);
+}
+
+console.log(totalEggs);
+collectEggs();
+console.log(totalEggs);
+
+let bird = 'Scarlet Macaw';
+function birdWatch() {
+    let bird = 'Great Blue Heron';
+    console.log(bird);
+}
+
+birdWatch();
+console.log(bird);
+
+// BLOCK SCOPE
+let radius = 8;
+
+if (radius > 0) {
+    const PI = 3.14159;
+    let msg = 'Hihi';
+}
+
+console.log(radius);
+console.log(PI);        // error
+
+for (let i = 0; i < 5; i++) {
+    let msg = "Hi";
+}
+console.log(msg); // error
+
+for (var i = 0; i < 5; i++) {
+    var msg = "Hi";
+}
+// VAR is function scoped, not block scoped
+// LET and CONST are function and block scoped
+console.log(i);   // 5  
+console.log(msg); // Hi
