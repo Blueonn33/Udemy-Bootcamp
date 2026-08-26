0ae56851c7b3e8369a030e838ef201fc136ff6fe
Blueonn33 <m.marinov1733@gmail.com>
2026-08-26 11:01:52 +0300

    function as an argument

--- js/scope/expressions.js
+++ js/scope/expressions.js
@@ -1,3 +1,30 @@
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
+
+add(3, 4);
+
+// FUNCTION EXPRESSION
+const add = function (x, y) {
+  return x + y;
+};
+
+add(4, 5);
+
+// HIGHER ORDER FUNCTIONS
+function callTwice(func) {
+  func();
+}
+
+function callTenTime(f) {
+    for (let i = 0; i < 10; i++) {
+        f();
+    }
+}
+
+function rollDie() {
+  const roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+}
+
+callTwice(rollDie);
