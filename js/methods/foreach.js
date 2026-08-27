f74bcd0984b59d3c00931a84d4247366cb6d8c91
Blueonn33 <m.marinov1733@gmail.com>
2026-08-27 11:52:30 +0300

    foreach

--- js/methods/foreach.js
+++ js/methods/foreach.js
@@ -1,3 +1,30 @@
 const numbers = [1, 2, 3, 4, 5, 5, 6, 6, 7, 5, 3, 3, 5];
+const movies = [
+    {
+        title: "Bleach TYBW",
+        score: 94
+    },
+    {
+        title: "Avatar",
+        score: 90
+    },
+    {
+        title: "John Wick",
+        score: 87
+    }
+];
 
+function print(el) {
+    console.log(el);
+}
+
+numbers.forEach(print)
+numbers.forEach(function(el) {
+    if (el % 2 === 0) 
+        console.log(el);
+});
+
+movies.forEach(function (movie) {
+    console.log(`${movie.title} - ${movie.score}/100`);
+});
 
