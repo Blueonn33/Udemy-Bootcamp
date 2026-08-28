3799e42abd0bd02a9b3c481d828523e14ac4ccfa
Blueonn33 <m.marinov1733@gmail.com>
2026-08-28 12:09:18 +0300

    array destructuring

--- js/features/destruct.js
+++ js/features/destruct.js
@@ -2,3 +2,12 @@
 //  - Values from arrays
 //  - Properties from objects 
 // Into distinct variables
+
+const scores = [97, 89, 78, 65, 60, 45];
+const highScore = scores[0];
+const secondHighScore = scores[1];
+
+const [gold, silver, bronze, ...everyoneElse] = scores;
+// gold   = 97
+// silver = 89
+
