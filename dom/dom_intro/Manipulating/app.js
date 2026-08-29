22514c24f53de3a3651ba50eaf6437d1f7540a62
Blueonn33 <m.marinov1733@gmail.com>
2026-08-29 16:12:29 +0300

    styles

--- dom/dom_intro/Manipulating/app.js
+++ dom/dom_intro/Manipulating/app.js
@@ -1,7 +1,15 @@
+const h1 = document.querySelector('h1');
+h1.style.color = "blue"
+
 const allLinks = document.querySelectorAll("a");
 
 for (let link of allLinks) {
-    link.innerText = "I AM A LINK";
+    link.style.color = "red";
+    link.style.textDecorationColor = "magenta";
+    link.style.textDecorationStyle = "wavy";
 }
 
-document.querySelector("h1").innerHTML = "<i>nmi</i>";
+// GETTING THE VALUES OF AN ELEMENT
+window.getComputedStyle(h1).color;
+window.getComputedStyle(h1).fontSize;
+window.getComputedStyle(h1).fontFamily;
