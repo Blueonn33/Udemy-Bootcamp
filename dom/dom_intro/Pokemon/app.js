170f1185e1d38371f4fa2af2506f65a963825f5e
Blueonn33 <m.marinov1733@gmail.com>
2026-08-29 21:58:35 +0300

    1025 pokemons

--- dom/dom_intro/Pokemon/app.js
+++ dom/dom_intro/Pokemon/app.js
@@ -1,19 +1,11 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const container = document.querySelector("#container");
 
+for (let i = 1; i <= 1025; i++) {
+    const src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
 
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
-
-
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+    const img = document.createElement("img");
+    img.setAttribute("src", src);
+    container.appendChild(img);
 }
