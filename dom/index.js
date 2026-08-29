22e50d88d5967c02c61ae81ad9503e272c47ef96
Blueonn33 <m.marinov1733@gmail.com>
2026-08-29 11:25:39 +0300

    dom intro

--- dom/index.js
+++ dom/index.js
@@ -3,3 +3,7 @@
 // JS window into the contents of a webpage
 // It is just a bunch of objects that you can interact with via JS
 
+// DOCUMENT - the document object is our entry point into the
+// world of the DOM. It contains representations of all the
+// content on a page, plus tons of useful methods and 
+// properties
