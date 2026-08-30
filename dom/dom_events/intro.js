6473e92018d7ff436587416e08678478a0c9fdf3
Blueonn33 <m.marinov1733@gmail.com>
2026-08-30 18:05:52 +0300

    event listener

--- dom/dom_events/intro.js
+++ dom/dom_events/intro.js
@@ -15,3 +15,22 @@ btn.onmouseenter = scream;
 // addEventListener - specify the event type and a callback
 // to run
 
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("click", function() {
+    alert("Clicked");
+})
+
+function twist() {
+    console.log("Twist");
+}
+function shout() {
+    console.log("shout");
+}
+
+const tasButton = document.querySelector("#tas");
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+// executes only once and removes the event listener
+tasButton.addEventListener("click", twist, {once: true});
+tasButton.addEventListener("click", shout);
