5e17db67f1507df0e18c91aa13bc8faad26f8d33
Blueonn33 <m.marinov1733@gmail.com>
2026-08-30 17:55:25 +0300

    eventListener

--- dom/dom_events/intro.js
+++ dom/dom_events/intro.js
@@ -1,2 +1,17 @@
 // EVENTS - responding to user inputs and actions
+// onclick
+const btn = document.querySelector("#v2");
+
+btn.onclick = function() {
+    console.log("You clicked me");
+}
+
+function scream() {
+    console.log("Aaaa");
+}
+
+btn.onmouseenter = scream;
+
+// addEventListener - specify the event type and a callback
+// to run
 
